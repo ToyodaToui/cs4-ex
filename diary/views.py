12eac82ec94e86747f3c9ef4a81430597a7fc1ac
Killa-Killa-Boshi <85663437+Killa-Killa-Boshi@users.noreply.github.com>
2023-01-11 23:42:47 +0900

    5
    5

--- diary/views.py
+++ diary/views.py
@@ -17,3 +17,33 @@ class DiaryCreateView(CreateView):
 
 class DiaryCreateCompleteView(TemplateView):
     template_name = 'diary_create_complete.html'
+
+from django.views.generic import ListView
+from .models import Diary
+class DiaryListView(ListView):
+    template_name = 'diary_list.html'
+    model = Diary
+ 
+from django.views.generic import DetailView   
+class DiaryDetailView(DetailView):
+    template_name = 'diary_detail.html'
+    model = Diary
+   
+from django.views.generic import UpdateView   
+class DiaryUpdateView(UpdateView):
+    template_name = 'diary_update.html'
+    model = Diary
+    fields = ('date', 'title', 'text',)
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.updated_at = timezone.now()
+        diary.save()
+        return super().form_valid(form)
+    
+from django.views.generic import DeleteView  
+class DiaryDeleteView(DeleteView):
+    template_name = 'diary_delete.html'
+    model = Diary
+    success_url = reverse_lazy('diary:diary_list')
